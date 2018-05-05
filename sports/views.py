629827b6eb1c9ac86cde1391d98ab0a4df761f42
rpSebastian <cxxuhang@126.com>
2018-05-05 12:40:23 +0800

    tutorial04/Write a simple form

--- sports/views.py
+++ sports/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
-from django.http import HttpResponse, Http404
-
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Athlete
 # Create your views here.
@@ -23,4 +23,13 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    athlete = get_object_or_404(Athlete, pk=question_id)
+    team = athlete.team
+    try:
+        friends = get_object_or_404(Athlete, athlete_id = request.POST['friends'])
+    except KeyError:
+        d1 = locals()
+        d1.update({'error_message' : "you do not select a friend!"})
+        return render(request, 'sports/details.html', d1)
+    else:
+        return HttpResponseRedirect(reverse('sports:results', args=(str(athlete.athlete_id))))
