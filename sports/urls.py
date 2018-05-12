07305301dc1704804f5374cdc39570a85797c8e4
albert666 <gwz1805504@gmail.com>
2018-05-12 18:41:07 +0800

    one comit

--- sports/urls.py
+++ sports/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-
+from django.conf.urls import url
 from . import views
+from django.contrib.auth.views import login,logout
+
 
 app_name = 'sports'
 urlpatterns = [
@@ -11,4 +13,5 @@ urlpatterns = [
     path('group/get_athlete_div/', views.get_athlete_div, name = "group_get_athlete_div"),
 
     path('group/register/submit', views.group_register_submit, name = "group_register_submit"),
+    url(r'^')
 ]
